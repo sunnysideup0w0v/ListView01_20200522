af7734c1a2f75b0d4ba70d2ff754276cc1556359
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-22 11:11:28 +0900

    studentClass의 뼈대작업. layout.studentList..넣어야함

--- app/src/main/java/com/example/listview01_20200522/adapters/StudentAdapter.java
+++ app/src/main/java/com/example/listview01_20200522/adapters/StudentAdapter.java
@@ -1,18 +1,37 @@
 package com.example.listview01_20200522.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
+import com.example.listview01_20200522.R;
 import com.example.listview01_20200522.datas.Student;
 
 import java.util.List;
 
 public class StudentAdapter extends ArrayAdapter<Student> {
-
+    Context mContext;
+    List<Student> mList;
+    LayoutInflater inf;
     public StudentAdapter(@NonNull Context context, int resource, @NonNull List<Student> objects) {
         super(context, resource, objects);
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+    }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if(row == null){
+            row = inf.inflate(R.layout., root:null);
+        }
+        return row;
     }
 }
