272c0da7c8e9b036d6b35d8aa75fa653bae85f74
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-22 11:33:47 +0900

    addStudents로 메소드를 별도로  만들어서, setValues에서는 addStudents를 실행만 시키기.

--- app/src/main/java/com/example/listview01_20200522/MainActivity.java
+++ app/src/main/java/com/example/listview01_20200522/MainActivity.java
@@ -29,6 +29,11 @@ public class MainActivity extends baseActivity {
 
     @Override
     public void setValues() {
+        addStudents();
+
+    }
+
+    void addStudents() {
         ourClassStudentList.add(new Student("조경진", "서울시 은평구"));
         ourClassStudentList.add(new Student("강희진", "인천시 남동구"));
         ourClassStudentList.add(new Student("구현정", "서울시 종로구"));
@@ -40,6 +45,6 @@ public class MainActivity extends baseActivity {
         ourClassStudentList.add(new Student("유혜원", "서울시 은평구"));
         ourClassStudentList.add(new Student("이다연", "서울시 은평구"));
         ourClassStudentList.add(new Student("황보석", "인천시 남동구"));
-
     }
+
 }
