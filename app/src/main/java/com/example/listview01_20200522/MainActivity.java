b339193d7915136deef503135991e499d168889d
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-22 11:32:33 +0900

    ourClassList Array 에 수강생 목록 담기

--- app/src/main/java/com/example/listview01_20200522/MainActivity.java
+++ app/src/main/java/com/example/listview01_20200522/MainActivity.java
@@ -29,7 +29,17 @@ public class MainActivity extends baseActivity {
 
     @Override
     public void setValues() {
+        ourClassStudentList.add(new Student("조경진", "서울시 은평구"));
+        ourClassStudentList.add(new Student("강희진", "인천시 남동구"));
         ourClassStudentList.add(new Student("구현정", "서울시 종로구"));
+        ourClassStudentList.add(new Student("김정민", "서울시 도봉구"));
+        ourClassStudentList.add(new Student("김하늬", "서울시 성북구"));
+        ourClassStudentList.add(new Student("문희원", "서울시 은평구"));
+        ourClassStudentList.add(new Student("송세인", "서울시 은평구"));
+        ourClassStudentList.add(new Student("위건", "서울시 성동구"));
+        ourClassStudentList.add(new Student("유혜원", "서울시 은평구"));
+        ourClassStudentList.add(new Student("이다연", "서울시 은평구"));
+        ourClassStudentList.add(new Student("황보석", "인천시 남동구"));
 
     }
 }
